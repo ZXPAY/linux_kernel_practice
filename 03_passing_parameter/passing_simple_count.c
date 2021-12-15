178960222d313a2cc3c793e4a9becce2c51aa079
Guillaume DannyDeng <zxpay73945788@gmail.com>
2021-12-15 22:09:01 +0800

    practice passing argument to kernel module

---
+++ 03_passing_parameter/passing_simple_count.c
@@ -0,0 +1,39 @@
+/*
+* Insert module:
+* $ sudo insmod passing_simple_count.ko count=100
+* Module information:
+* $ sudo modinfo ./passing_simple_count.ko
+* See the message:
+* $ sudo dmesg
+*/
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+
+char *name = "Danny";
+int count = 0;
+module_param(name, charp, S_IRUGO);
+module_param(count, int, S_IRUGO);
+
+
+static int test_arguments_init(void) {
+    printk(KERN_INFO"%s: In init\n", __func__);
+    printk(KERN_INFO"%s: pass count: %d\n", __func__, count);
+
+    return 0;
+}
+
+
+static void test_exit(void) {
+    printk(KERN_INFO"%s: In exit: \n", __func__);
+}
+
+
+
+module_init(test_arguments_init);
+module_exit(test_exit);
+
+MODULE_AUTHOR("Danny Deng");
+MODULE_DESCRIPTION("Argument parsing example");
